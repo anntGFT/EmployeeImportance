ef0383a153b1561dbd9de4fa2966090820c299d1
annt <annt@gft.com>
2022-12-19 17:00:37 +0000

    Done wit recursion and stream

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,25 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int getImportance(List<Employee> employees, int id) {
+
+        return recursionMethod(id, employees.stream().
+                collect(Collectors.toMap(e->e.id, e->e)));
+    }
+
+    private int recursionMethod(int id, Map<Integer, Employee> directory){
+        return  directory.get(id).importance +
+                directory.get(id).subordinates
+                        .stream()
+                        .mapToInt(e -> recursionMethod(e,directory))
+                        .sum();
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,67 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int result, id;
+    static List<Employee> employees;
+    static List<Integer> subordinates;
+
+    Employee e1,e2,e3;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+        employees = new ArrayList<>();
+        subordinates = new ArrayList<>();
+    }
+
+    @AfterEach
+    void after(){
+
+        employees.clear();
+        subordinates.clear();
+    }
+
+    @Test
+    public void twoEmployees(){
+
+        result =-3;
+        id = 5;
+        subordinates.add(5);
+        e1 = new Employee(1,5, subordinates);
+        e2 = new Employee(5,-3, new ArrayList<>());
+        employees.add(e1);
+        employees.add(e2);
+
+        assertEquals(result, solution.getImportance(employees, id));
+    }
+
+    @Test
+    public void threeEmployees(){
+
+        result = 11;
+        id = 1;
+        subordinates.add(2);
+        subordinates.add(3);
+        e1 = new Employee(1,5, subordinates);
+        e2 = new Employee(2,3, new ArrayList<>());
+        e3 = new Employee(3,3, new ArrayList<>());
+        employees.add(e1);
+        employees.add(e2);
+        employees.add(e3);
+
+        assertEquals(result, solution.getImportance(employees, id));
+    }
 }
